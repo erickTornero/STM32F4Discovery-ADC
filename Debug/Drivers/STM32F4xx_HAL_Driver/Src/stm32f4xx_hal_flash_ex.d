208ea2659a5b3d51aee9ab9b353df3e0bb73c60e
erickTornero <erickdeivy01@gmail.com>
2017-05-09 18:22:24 -0500

    Initial commit

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.d
@@ -0,0 +1,83 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /home/limabionics/workspace/ADC_8Channels/Inc/stm32f4xx_hal_conf.h \
+ /home/limabionics/workspace/ADC_8Channels/Inc/main.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Include/core_cm4.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Include/core_cmSimd.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/home/limabionics/workspace/ADC_8Channels/Inc/stm32f4xx_hal_conf.h:
+
+/home/limabionics/workspace/ADC_8Channels/Inc/main.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/home/limabionics/workspace/ADC_8Channels/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
---
+++ Readme.md
@@ -0,0 +1,8 @@
+This proyect uses ADC's of STM32F4Discovery,
+Using HAL interface
+Configurations:
+GPIOS:
+	-ADC - 8 Channels  PIN A0 - A7 or channel 0-7
+Configuration ADC Mode:
+	-Scan sample - time sampling: 3 clock
+	-Continuous sampling  with Direct Access Memory -DMA		
